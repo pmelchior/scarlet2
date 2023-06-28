cf70bc93ffbf0326eafc82fc37a4a5ba66410f3f
charlotteaward <charlotte.ward42@gmail.com>
2023-06-28 17:14:12 -0400

    Adding staticsource

--- scarlet2/source.py
+++ scarlet2/source.py
@@ -1,8 +1,8 @@
 from .module import Module, Parameter
 from .morphology import Morphology, GaussianMorphology
 from .scene import Scenery
-from .spectrum import Spectrum
-
+from .spectrum import Spectrum,Specind
+import jax.numpy as jnp
 
 class Source(Module):
     spectrum: Spectrum
@@ -30,6 +30,35 @@ class Source(Module):
     @property
     def bbox(self):
         return self.spectrum.bbox @ self.morphology.bbox
+
+class StaticSource(Module):
+    spectrum: Spectrum
+    morphology: Morphology 
+    specind: Specind
+    def __init__(self, center, spectrum, morphology, repeats):
+        self.spectrum = spectrum
+        self.morphology = morphology
+        if not isinstance(center, Parameter):
+            center = Parameter(center, fixed=True)
+        self.morphology.set_center(center) 
+        self.specind = jnp.repeat(jnp.arange(spectrum.value.shape[0]),repeats)
+        
+        # add this source to the active scene
+        try:
+            Scenery.scene.sources.append(self)
+        except AttributeError:
+            print("Sources can only be created within the context of a Scene")
+            print("Use 'with Scene(frame) as scene: Source(...)'")
+            raise
+
+    def __call__(self):
+        # Boxed model
+        return self.spectrum()[self.specind][:, None, None] * self.morphology()[None, :, :]
+
+    @property
+    def bbox(self): 
+        return self.spectrum.bbox @ self.morphology.bbox
+
 
 
 class PointSource(Source):
--- scarlet2/spectrum.py
+++ scarlet2/spectrum.py
@@ -1,12 +1,21 @@
 import equinox as eqx
 from .bbox import Box
 from .module import Parameter, Module
+import jax.numpy as jnp
 
 class Spectrum(Module):
     bbox: Box = eqx.static_field()
 
+class Specind(Module):
+    bbox: Box = eqx.static_field()
 
 class ArraySpectrum(Spectrum, Parameter):
     def __init__(self, *args, **kwargs):
         Parameter.__init__(self, *args, **kwargs)
         self.bbox = Box(self.value.shape)
+
+class StaticArraySpectrum(Spectrum, Parameter):
+    def __init__(self, *args, **kwargs):
+        Parameter.__init__(self, *args, **kwargs)  
+        self.bbox = Box(jnp.repeat(self.value,self.repeats).shape)
+
