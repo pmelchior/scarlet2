64d24b98b588983df8bdd522177eaf8b99595bae
Peter Melchior <peter.m.melchior@gmail.com>
2023-06-30 23:14:37 -0400

    simplified imports; tqdm

--- scarlet2/scene.py
+++ scarlet2/scene.py
@@ -45,105 +45,97 @@ class Scene(Module):
     def __exit__(self, exc_type, exc_value, traceback):
         Scenery.scene = None
 
-    def fit(self, observations, max_iter=100, verbose=False):
+    def fit(self, observations, max_iter=100, progress=True, **kwargs):
         try:
+            import tqdm
             import optax
+            from numpyro.distributions.transforms import biject_to
         except ImportError:
-            raise ImportError("scarlet2.Scene.fit() requires optax. Please install optax.")
+            raise ImportError("scarlet2.Scene.fit() requires optax and numpyro.")
 
-        optim, scene_, obs_, filter_spec, constraint_fn, opt_state = _init_optim(self, observations, learning_rate=1e-2)
-        for step in range(max_iter):
-            scene_, loss, opt_state = _make_step(scene_, obs_, optim, opt_state, filter_spec=filter_spec,
-                                                 constraint_fn=constraint_fn)
-            if verbose:
-                print(f"step={step}, loss={loss.item()}")
-
-        return _constraint_replace(scene_, constraint_fn)
-
-
-# provide default fitter if optax is present
-try:
-    import optax
-    from numpyro.distributions.transforms import biject_to
-
-
-    def _constraint_replace(self, constraint_fn, inv=False):
-        parameters = self.get_parameters(return_info=True)
-        names = tuple(name
-                      for name, (value, info) in parameters.items()
-                      if info["constraint"] is not None
-                      )
-
-        transform = lambda value, fn: fn(value)
-        inv_transform = lambda value, fn: fn.inv(value)
-        transform = (transform, inv_transform)
-        values = tuple(transform[inv](value, constraint_fn[name])
-                       for name, (value, info) in parameters.items()
-                       if info["constraint"] is not None
-                       )
-        return self.replace(names, values)
-
-
-    @eqx.filter_jit
-    def _make_step(model, observations, optim, opt_state, filter_spec=None, constraint_fn=None):
-        parameters = model.get_parameters(return_info=True)
-
-        def loss_fn(model):
-            if constraint_fn is not None:
-                # parameters now obey constraints
-                model = _constraint_replace(model, constraint_fn)
-
-            pred = model()
-            log_like = sum(obs.log_likelihood(pred) for obs in observations)
-            log_prior = sum(info["prior"].log_prob(p)
-                            for name, (p, info) in parameters.items()
-                            if info["prior"] is not None
-                            )
-            return -(log_like + log_prior)
-
-        if filter_spec is None:
-            loss, grads = eqx.filter_value_and_grad(loss_fn)(model)
-        else:
-            @eqx.filter_value_and_grad
-            def filtered_loss_fn(diff_model, static_model):
-                model = eqx.combine(diff_model, static_model)
-                return loss_fn(model)
-
-            diff_model, static_model = eqx.partition(model, filter_spec)
-            loss, grads = filtered_loss_fn(diff_model, static_model)
-
-        updates, opt_state = optim.update(grads, opt_state)
-        model = eqx.apply_updates(model, updates)
-        return model, loss, opt_state
-
-
-    def _init_optim(self, obs, optim=None, **kwargs):
         # dealing with multiple observations
-        if not isinstance(obs, (list, tuple)):
-            obs_ = (obs,)
+        if not isinstance(observations, (list, tuple)):
+            obs_ = (observations,)
         else:
-            obs_ = obs
+            obs_ = observations
 
         # set up optimimzer
         # TODO: different step sizes for optimizer
         # see https://github.com/deepmind/optax/blob/7063ce8022ce6165926de369d6fefa207e318127/optax/_src/combine.py#L65
-        if optim is None:
-            optim = optax.adam(**kwargs)
+        learning_rate = kwargs.pop("learning_rate", 1e-2)
+        optim = optax.adam(learning_rate=learning_rate, **kwargs)
 
         # transform to unconstrained parameters
         parameters = self.get_parameters(return_info=True)
         constraint_fn = {name: biject_to(info["constraint"]) for name, (value, info) in parameters.items() if
                          info["constraint"] is not None}
-        model_ = _constraint_replace(self, constraint_fn, inv=True)
+        scene_ = _constraint_replace(self, constraint_fn, inv=True)
 
         # get optimizer set up with the filtered (and unconstrained) parameters
         filter_spec = self.filter_spec
         if filter_spec is None:
-            opt_state = optim.init(model_)
+            opt_state = optim.init(scene_)
         else:
-            opt_state = optim.init(eqx.filter(model_, filter_spec))
+            opt_state = optim.init(eqx.filter(scene_, filter_spec))
 
-        return optim, model_, obs_, filter_spec, constraint_fn, opt_state
+        with tqdm.trange(max_iter) as t:
+            for step in t:
+                # optimizer step
+                scene_, loss, opt_state = _make_step(scene_, obs_, optim, opt_state, filter_spec=filter_spec,
+                                                     constraint_fn=constraint_fn)
+                # Log the loss in the tqdm progress bar
+                t.set_postfix(loss=f"{loss:08.2f}")
 
-except ImportError:
-    pass
+        return _constraint_replace(scene_, constraint_fn)  # transform back to constrained variables
+
+
+def _constraint_replace(self, constraint_fn, inv=False):
+    # replace any parameter with constraints into unconstrained ones by calling constraint_fn
+    # return transformed pytree
+    parameters = self.get_parameters(return_info=True)
+    names = tuple(name
+                  for name, (value, info) in parameters.items()
+                  if info["constraint"] is not None
+                  )
+    transform = lambda value, fn: fn(value)
+    inv_transform = lambda value, fn: fn.inv(value)
+    transform = (transform, inv_transform)
+    values = tuple(transform[inv](value, constraint_fn[name])
+                   for name, (value, info) in parameters.items()
+                   if info["constraint"] is not None
+                   )
+    return self.replace(names, values)
+
+
+# update step for optax optimizer
+@eqx.filter_jit
+def _make_step(model, observations, optim, opt_state, filter_spec=None, constraint_fn=None):
+    parameters = model.get_parameters(return_info=True)
+
+    def loss_fn(model):
+        if constraint_fn is not None:
+            # parameters now obey constraints
+            model = _constraint_replace(model, constraint_fn)
+
+        pred = model()
+        log_like = sum(obs.log_likelihood(pred) for obs in observations)
+        log_prior = sum(info["prior"].log_prob(p)
+                        for name, (p, info) in parameters.items()
+                        if info["prior"] is not None
+                        )
+        return -(log_like + log_prior)
+
+    if filter_spec is None:
+        loss, grads = eqx.filter_value_and_grad(loss_fn)(model)
+    else:
+        @eqx.filter_value_and_grad
+        def filtered_loss_fn(diff_model, static_model):
+            model = eqx.combine(diff_model, static_model)
+            return loss_fn(model)
+
+        diff_model, static_model = eqx.partition(model, filter_spec)
+        loss, grads = filtered_loss_fn(diff_model, static_model)
+
+    updates, opt_state = optim.update(grads, opt_state)
+    model = eqx.apply_updates(model, updates)
+    return model, loss, opt_state
