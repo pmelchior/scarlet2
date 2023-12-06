392dac62c292e590f83b6a325ecab72441a0bd76
jaredcsiegel <59315830+jaredcsiegel@users.noreply.github.com>
2023-12-06 15:09:14 -0500

    Added iteration number dependence to relative step

--- scarlet2/scene.py
+++ scarlet2/scene.py
@@ -147,7 +147,7 @@ class Scene(Module):
                 if params is None:
                     raise ValueError(base.NO_PARAMS_MSG)
                 updates = jax.tree_util.tree_map(
-                    lambda u, step, param: -step * u if not callable(step) else -step(param) * u,
+                    lambda u, step, param: -step * u if not callable(step) else -step(param,niter) * u,
                     # minus because we want gradient descent
                     updates, steps, params)
                 return updates, state
@@ -173,6 +173,7 @@ class Scene(Module):
             opt_state = optim.init(eqx.filter(scene, filter_spec))
 
         with tqdm.trange(max_iter, disable=not progress_bar) as t:
+            niter = 0
             for step in t:
                 # optimizer step
                 scene_, loss, opt_state = _make_step(scene, observations, optim, opt_state, filter_spec=filter_spec,
@@ -193,7 +194,7 @@ class Scene(Module):
                         break
 
                 scene = scene_
-
+                niter += 1
         return _constraint_replace(scene_, constraint_fn)  # transform back to constrained variables
 
 
