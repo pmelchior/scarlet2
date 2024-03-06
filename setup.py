98ba13e6a2717a1667b8eaa1e4f4c771677697ef
Benjamin Remy <remy.benjamin.101@gmail.com>
2024-03-06 16:10:25 +0100

    adding setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='scarlet2',
+    version='0.1.0',    
+    description='Scarlet, all new and shiny',
+    url='https://github.com/pmelchior/scarlet2',
+    author='Peter Melchior',
+    author_email='melchior@astro.princeton.edu',
+    license='MIT',
+    packages=['scarlet2'],
+    install_requires=[
+        'equinox',
+        'astropy',
+        'numpy',
+        'matplotlib'
+    ],
+)
