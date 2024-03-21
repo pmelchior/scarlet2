464f807a692ad5488d48165eaac1eb9a6c7acce8
Benjamin Remy <30293694+b-remy@users.noreply.github.com>
2024-03-21 10:34:25 -0400

    Lanczos resampling renderer (#43)
    * renderers as eqx.nn.Sequential
    
    * adding setup.py for pip install
    
    * adding deconv/resampling/reconv renderers (fft)
    
    * resampling with rfft
    
    * adding pixel_size in arcsec attribute
    
    * add switches in renderers
    
    * black formating
    
    * use 4x padding
    
    * black
    
    * adding jax to setup.py
    
    * update lancsoz k <1e-4
    
    * fix Frame.bbox with shape only in from_observations
    
    ---------
    
    Co-authored-by: Peter Melchior <peter.m.melchior@gmail.com>

---
+++ scarlet2/interpolation.py
@@ -0,0 +1,114 @@
+import jax
+import jax.numpy as jnp
+
+
+def f_1(x, n):
+    """
+    Approximation from galsim
+    // res = n/(pi x)^2 * sin(pi x) * sin(pi x / n)
+    //     ~= (1 - 1/6 pix^2) * (1 - 1/6 pix^2 / n^2)
+    //     = 1 - 1/6 pix^2 ( 1 + 1/n^2 )
+    """
+    px = jnp.pi * x
+    temp = 1./6. * px * px
+    res = 1. - temp * (1. + 1. / (n * n))
+    return res
+
+
+def f_2(x, n):
+    px = jnp.pi * x
+    return n * jnp.sin(px) * jnp.sin(px / n) / px / px
+
+
+from functools import partial
+
+
+def lanczos_n(x, n=3):
+    """
+    Lanczos interpolation kernel
+
+    Parameters
+    ----------
+    n: Lanczos order
+    """
+    small_x = jnp.abs(x) <= 1e-4
+    window_n = jnp.abs(x) <= n
+
+    return jnp.piecewise(
+        x, [small_x, (~small_x) & window_n], [f_1, f_2, lambda x, n: jnp.array(0)], n
+    )
+
+
+def resample2d(signal, coords, warp, n=3):
+    """
+    Resample a 2-dimensional image using a Lanczos kernel
+
+    Parameters
+    ----------
+    signal: array
+        2d array containing the signal. We assume here that the coordinates of
+        the signal
+        shape: [Nx, Ny]
+    coords: array
+        Coordinates on which the signal is sampled.
+        shape: [Nx, Ny, 2]
+            - x-coordinates are coords[0,:,0]
+            - y-coordinates are coords[:,0,1]
+    warp: array
+        Coordinates on which to resample the signal.
+        shape:[nx, ny, 2]
+        [ [[0,  0], [0,  1], ...,  [0,  N-1]],
+                           [ ... ],
+          [[N-1,0], [N-1,1], ...,  [N-1,N  ]] ]
+    n: Lanczos order
+
+    Returns
+    -------
+    resampled_signal: array
+    """
+
+    x = warp[..., 0].flatten()
+    y = warp[..., 1].flatten()
+
+    coords_x = coords[0, :, 0]
+    coords_y = coords[:, 0, 1]
+
+    h = coords_x[1] - coords_x[0]
+
+    xi = jnp.floor((x - coords_x[0]) / h).astype(jnp.int32)
+    yi = jnp.floor((y - coords_y[0]) / h).astype(jnp.int32)
+
+    Nx = coords.shape[0]
+    Ny = coords.shape[1]
+
+    def body_fun_x(i, args):
+        res, yind, ky, masky = args
+
+        xind = xi + i
+        maskx = (xind >= 0) & (xind < Ny)
+
+        kx = lanczos_n((x - coords_x[xind]) / h, n)
+
+        k = kx * ky
+        mask = maskx & masky
+        res += jnp.where(mask, k * signal[yind, xind], 0)
+
+        return res, yind, ky, masky
+
+    def body_fun_y(i, args):
+        res = args
+
+        yind = yi + i
+        masky = (yind >= 0) & (yind < Nx)
+
+        ky = lanczos_n((y - coords_y[yind]) / h, n)
+
+        res = jax.lax.fori_loop(-n, n + 1, body_fun_x, (res, yind, ky, masky))[0]
+
+        return res
+
+    res = jax.lax.fori_loop(
+        -n, n + 1, body_fun_y, jnp.zeros_like(x).astype(signal.dtype)
+    )
+
+    return res.reshape(warp[..., 0].shape)
--- scarlet2/renderer.py
+++ scarlet2/renderer.py
@@ -1,38 +1,50 @@
 import equinox as eqx
 import jax.numpy as jnp
+import jax
 
-from .fft import convolve, deconvolve, _get_fast_shape
+from .fft import convolve, deconvolve, _get_fast_shape, transform, inverse
+
+from .interpolation import resample2d
 
 
 class Renderer(eqx.Module):
+    def __call__(
+        self, model, key=None
+    ):  # key is needed to chain renderers with eqx.nn.Sequential
+        raise NotImplementedError
+
+
+class NoRenderer(Renderer):
+    def __call__(self, model, key=None):
+        return model
+
+
+class ChannelRenderer(Renderer):
     channel_map: (None, list, slice) = None
 
     def __init__(self, model_frame, obs_frame):
-        self.channel_map = self.get_channel_map(model_frame, obs_frame)
-
-    def __call__(self, model):
-        raise NotImplementedError
-
-    def get_channel_map(self, model_frame, obs_frame):
         if obs_frame.channels == model_frame.channels:
-            return None
-        try:
-            channel_map = [
-                list(model_frame.channels).index(c) for c in list(obs_frame.channels)
-            ]
-        except ValueError:
-            msg = "Cannot match channels between model and observation.\n"
-            msg += f"Got {model_frame.channels} and {obs_frame.channels}."
-            raise ValueError(msg)
+            channel_map = None
+        else:
+            try:
+                channel_map = [
+                    list(model_frame.channels).index(c)
+                    for c in list(obs_frame.channels)
+                ]
+            except ValueError:
+                msg = "Cannot match channels between model and observation.\n"
+                msg += f"Got {model_frame.channels} and {obs_frame.channels}."
+                raise ValueError(msg)
 
-        min_channel = min(channel_map)
-        max_channel = max(channel_map)
-        if max_channel + 1 - min_channel == len(channel_map):
-            channel_map = slice(min_channel, max_channel + 1)
-        return channel_map
+            min_channel = min(channel_map)
+            max_channel = max(channel_map)
+            if max_channel + 1 - min_channel == len(channel_map):
+                channel_map = slice(min_channel, max_channel + 1)
+        self.channel_map = channel_map
 
-    def map_channels(self, model):
-        """Map to model channels onto the observation channels
+    def __call__(self, model, key=None):
+        """Map model channels onto the observation channels
+
         Parameters
         ----------
         model: array
@@ -50,18 +62,8 @@ class Renderer(eqx.Module):
         return jnp.dot(self.channel_map, model)
 
 
-class NoRenderer(Renderer):
-    def __init__(self):
-        self.channel_map = None
-
-    def __call__(self, model):
-        return model
-
-
 class ConvolutionRenderer(Renderer):
     def __init__(self, model_frame, obs_frame):
-        super().__init__(model_frame, obs_frame)
-
         # create PSF model
         psf = model_frame.psf()
         if len(psf.shape) == 2:  # only one image for all bands
@@ -69,12 +71,149 @@ class ConvolutionRenderer(Renderer):
         else:
             psf_model = psf
         # make sure fft uses a shape large enough to cover the convolved model
-        fft_shape = _get_fast_shape(model_frame.bbox.shape, psf_model.shape, padding=3, axes=(-2, -1))
+        fft_shape = _get_fast_shape(
+            model_frame.bbox.shape, psf_model.shape, padding=3, axes=(-2, -1)
+        )
+
         # compute and store diff kernel in Fourier space
-        diff_kernel_fft = deconvolve(obs_frame.psf(), psf_model, axes=(-2, -1), fft_shape=fft_shape, return_fft=True)
+        diff_kernel_fft = deconvolve(
+            obs_frame.psf(),
+            psf_model,
+            axes=(-2, -1),
+            fft_shape=fft_shape,
+            return_fft=True,
+        )
         object.__setattr__(self, "_diff_kernel_fft", diff_kernel_fft)
 
-    def __call__(self, model):
-        # TODO: including slices
-        model_ = self.map_channels(model)
-        return convolve(model_, self._diff_kernel_fft, axes=(-2, -1))
+    def __call__(self, model, key=None):
+        return convolve(model, self._diff_kernel_fft, axes=(-2, -1))
+
+
+class KDeconvRenderer(Renderer):
+    """
+    Perform decovolution in Fourier space and return the Fourier decovolved
+    model
+    """
+
+    def __init__(self, model_frame, padding=None):
+
+        if padding == None:
+            # use 4 times padding
+            pad_factor = 4
+            padding = pad_factor * max(
+                model_frame.bbox.shape[1], model_frame.bbox.shape[2]
+            )
+        object.__setattr__(self, "_pad_size", padding)
+
+        # create PSF model
+        psf = model_frame.psf()
+        if len(psf.shape) == 2:  # only one image for all bands
+            psf_model = jnp.tile(psf, (model_frame.bbox.shape[0], 1, 1))
+        else:
+            psf_model = psf
+        object.__setattr__(self, "_psf_model", psf_model)
+
+    def __call__(self, model, key=None):
+        psf_model = self._psf_model
+        fft_shape = _get_fast_shape(
+            model.shape, psf_model.shape, padding=self._pad_size, axes=(-2, -1)
+        )
+        deconv_ = deconvolve(
+            model, psf_model, axes=(-2, -1), fft_shape=fft_shape, return_fft=True
+        )
+
+        return deconv_
+
+
+class KResampleRenderer(Renderer):
+    """
+    Perform resampling in Fourier space
+
+    Should get as input a Fourier image sampled at the model frame resolution
+    Needs to return a Fourier image sampled at the observation frame resolution
+    """
+
+    def __init__(self, model_frame, obs_frame, padding=None):
+        object.__setattr__(self, "_in_res", model_frame.pixel_size)
+        object.__setattr__(self, "_out_res", obs_frame.pixel_size)
+
+        if padding == None:
+            # use 4 times padding
+            pad_factor = 4
+            padding = pad_factor * max(obs_frame.bbox.shape[1], obs_frame.bbox.shape[2])
+
+        # find on what grid we will interpolate
+        fft_out_shape = _get_fast_shape(
+            obs_frame.bbox.shape, obs_frame.psf().shape, padding=padding, axes=(-2, -1)
+        )
+        object.__setattr__(self, "_fft_out_shape", fft_out_shape)
+
+        # compute resolution ratio for flux conservation
+        object.__setattr__(
+            self, "_resolution_ratio", obs_frame.pixel_size / model_frame.pixel_size
+        )
+
+    def __call__(self, kimage, key=None):
+
+        # compute model k-coordinates of the fourier input image
+        ky_in = jnp.linspace(-0.5, 0.5, kimage.shape[1]) / self._in_res
+        kx_in = jnp.linspace(0, 0.5, kimage.shape[1] // 2 + 1) / self._in_res
+
+        ky_out = jnp.linspace(-0.5, 0.5, self._fft_out_shape[0]) / self._out_res
+        kx_out = jnp.linspace(0, 0.5, self._fft_out_shape[0] // 2 + 1) / self._out_res
+
+        kimage = jnp.fft.fftshift(kimage, -2)
+
+        kcoords_in = jnp.stack(jnp.meshgrid(kx_in, ky_in), -1)
+        kcoords_out = jnp.stack(jnp.meshgrid(kx_out, ky_out), -1)
+
+        k_resampled = jax.vmap(resample2d, in_axes=(0, None, None, None))(
+            kimage, kcoords_in, kcoords_out, 3
+        )
+
+        k_resampled = jnp.fft.ifftshift(k_resampled, -2)
+
+        # conserve flux
+        k_resampled = k_resampled * self._resolution_ratio
+
+        return k_resampled
+
+
+class KConvolveRenderer(Renderer):
+    """
+    Convolve with obs PSF and return real image
+    """
+
+    def __init__(self, obs_frame, padding=None):
+        object.__setattr__(self, "_obs_shape", obs_frame.bbox.shape)
+
+        if padding == None:
+            # use 4 times padding
+            pad_factor = 4
+            padding = pad_factor * max(obs_frame.bbox.shape[1], obs_frame.bbox.shape[2])
+        object.__setattr__(self, "_pad_size", padding)
+
+        # get PSF from obs
+        psf = obs_frame.psf()
+        if len(psf.shape) == 2:  # only one image for all bands
+            psf_model = jnp.tile(psf, (obs_frame.bbox.shape[0], 1, 1))
+        else:
+            psf_model = psf
+        object.__setattr__(self, "_psf_model", psf_model)
+
+        fft_out_shape = _get_fast_shape(
+            obs_frame.bbox.shape, obs_frame.psf().shape, padding=padding, axes=(-2, -1)
+        )
+        object.__setattr__(self, "_fft_out_shape", fft_out_shape)
+
+    def __call__(self, kimage, key=None):
+
+        # Convolve with obs PSF
+
+        kpsf = transform(self._psf_model, self._fft_out_shape, (-2, -1))
+
+        kconv = kimage * kpsf
+
+        img = inverse(kconv, self._fft_out_shape, self._obs_shape, axes=(-2, -1))
+
+        return img
