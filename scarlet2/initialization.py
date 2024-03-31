cd535cba2873b70f8e336ff5fd60ae2b9409db08
Matt <matt.sampson@princeton.edu>
2024-03-31 12:46:59 -0400

    reformatted with black

--- scarlet2/initialization.py
+++ scarlet2/initialization.py
@@ -119,6 +119,7 @@ def fit_morph_params(data, center, bx):
     best_params = lbfgsb.run(x0, bounds=bounds).params
     return best_params
 
+
 # take the 3 moments of a @D gaussian and construct the covariance matrix to fit the shape
 
 
@@ -315,10 +316,10 @@ def init_spectrum(observations, center, correct_psf=None, models=None):
 
         spectra.append(spectrum)
 
-        #if jnp.any(spectrum <= 0):
-            # If the flux in all channels is  <=0,
-            # the new sed will be filled with NaN values,
-            # which will cause the code to crash later
+        # if jnp.any(spectrum <= 0):
+        # If the flux in all channels is  <=0,
+        # the new sed will be filled with NaN values,
+        # which will cause the code to crash later
         #    msg = f"Zero or negative spectrum {spectrum} at {center}"
         #    if np.all(spectrum <= 0):
         #        logger.warning(msg)
