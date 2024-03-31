a1a9b257cd8d02cc52564e3a59de130ae6cceb1b
Peter Melchior <peter.m.melchior@gmail.com>
2024-03-30 20:01:28 -0400

    Combined sources (#44)
    * added Component and DustComponent; source hierarchy
    
    * bugfix when WCS is not set
    
    * add import; return self after adding components
    
    * get_parameters() now more robust for non-Parameter tuples

--- scarlet2/source.py
+++ scarlet2/source.py
@@ -1,12 +1,18 @@
 import copy
+import operator
 
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+
+from .bbox import overlap_slices
 from .module import Module
 from .morphology import Morphology
 from .scene import Scenery
 from .spectrum import Spectrum
 
 
-class Source(Module):
+class Component(Module):
     spectrum: Spectrum
     morphology: Morphology
 
@@ -16,6 +22,31 @@ class Source(Module):
         self.morphology.center_bbox(center)
         super().__post_init__()
 
+    def __call__(self):
+        # Boxed model
+        return self.spectrum()[:, None, None] * self.morphology()[None, :, :]
+
+    @property
+    def bbox(self):
+        return self.spectrum.bbox @ self.morphology.bbox
+
+
+class DustComponent(Component):
+    def __call__(self):
+        return jnp.exp(-super().__call__())
+
+
+class Source(Component):
+    components: list
+    component_ops: list = eqx.field(static=True)
+
+    def __init__(self, center, spectrum, morphology):
+        # set the base component
+        super().__init__(center, spectrum, morphology)
+        # create the empty component list
+        self.components = list()
+        self.component_ops = list()
+
         # add this source to the active scene
         try:
             Scenery.scene.sources.append(self)
@@ -24,13 +55,48 @@ class Source(Module):
             print("Use 'with Scene(frame) as scene: Source(...)'")
             raise
 
-    def __call__(self):
-        # Boxed model
-        return self.spectrum()[:, None, None] * self.morphology()[None, :, :]
+    def add_component(self, component, op):
+        assert isinstance(component, (Source, Component))
 
-    @property
-    def bbox(self):
-        return self.spectrum.bbox @ self.morphology.bbox
+        # if component is a source, it's already registered in scene
+        # remove it from scene to not call it twice
+        if isinstance(component, Source):
+            try:
+                Scenery.scene.sources.remove(component)
+            except AttributeError:
+                print("Source can only be modified within the context of a Scene")
+                print("Use 'with Scene(frame) as scene: Source(...)'")
+                raise
+            except ValueError:
+                pass
+
+        # adding a full source will maintain its ownership of components:
+        # hierarchical definition of sources withing sources
+        self.components.append(component)
+        self.component_ops.append(op)
+        return self
+
+    def __iadd__(self, component):
+        return self.add_component(component, operator.add)
+
+    def __imul__(self, component):
+        return self.add_component(component, operator.mul)
+
+    def __call__(self):
+        base = super()
+        model = base.__call__()
+        for component, op in zip(self.components, self.component_ops):
+            model_ = component()
+            # cut out regions from model and model_
+            bbox, bbox_ = overlap_slices(base.bbox, component.bbox, return_boxes=True)
+            sub_model = jax.lax.dynamic_slice(model, bbox.start, bbox.shape)
+            sub_model_ = jax.lax.dynamic_slice(model_, bbox_.start, bbox_.shape)
+            # combine with operator
+            sub_model = op(sub_model, sub_model_)
+            # add model_ back in full model
+            model = jax.lax.dynamic_update_slice(model, sub_model, bbox.start)
+        return model
+
 
 class PointSource(Source):
     def __init__(self, center, spectrum):
