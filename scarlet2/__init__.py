4b4bb9838b2eef3baee10977db77d6f3ae5b06ec
Peter Melchior <peter.m.melchior@gmail.com>
2024-05-08 10:46:36 -0400

    proper module level import

--- scarlet2/__init__.py
+++ scarlet2/__init__.py
@@ -1,5 +1,6 @@
+from . import initialization as init
+from . import measure
 from . import plot
-from .initialization import *
 from .bbox import Box
 from .frame import Frame
 from .module import Parameter, Module, relative_step
@@ -9,4 +10,3 @@ from .psf import PSF, ArrayPSF, GaussianPSF
 from .scene import Scene
 from .source import Component, DustComponent, Source, PointSource
 from .spectrum import Spectrum, ArraySpectrum, StaticArraySpectrum, TransientArraySpectrum
-from .measure import * 
