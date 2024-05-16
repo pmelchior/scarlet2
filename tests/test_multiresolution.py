8f3c935fab5719a215ce357cffa2ee7bf777a37f
Benjamin Remy <remy.benjamin.101@gmail.com>
2024-05-16 17:14:41 -0400

    relative path + pip install git+scarlet-test-data

--- tests/test_multiresolution.py
+++ tests/test_multiresolution.py
@@ -1,9 +1,15 @@
+import os
 import warnings
 warnings.filterwarnings('ignore')
 
 import numpy as np
 from numpy.testing import assert_allclose
+
 import scarlet2
+
+from utils import import_scarlet_test_data
+import_scarlet_test_data()
+from scarlet_test_data import data_path, tests_path
 
 import numpy as np
 import astropy.io.fits as fits
@@ -12,25 +18,28 @@ from astropy.wcs import WCS
 import jax.numpy as jnp
 
 # Load the HSC image data
-obs_hdu = fits.open('../scarlet-test-data/data/test_resampling/Cut_HSC1.fits')
+obs_hdu = fits.open(os.path.join(data_path, "test_resampling", "Cut_HSC1.fits"))
+                    
 data_hsc = jnp.array(obs_hdu[0].data.byteswap().newbyteorder(), jnp.float32)
 wcs_hsc = WCS(obs_hdu[0].header)
 channels_hsc = ['g','r','i','z','y']
 
 # Load the HSC PSF data
-psf_hsc_data = fits.open('../scarlet-test-data/data/test_resampling/PSF_HSC.fits')[0].data.astype('float32')
+psf_hsc_data  = fits.open(os.path.join(data_path, "test_resampling", "PSF_HSC.fits"))[0].data.astype('float32')
+                    
 Np1, Np2 = psf_hsc_data[0].shape
 psf_hsc_data = jnp.pad(psf_hsc_data, ((0,0), (1,0), (1,0)))
 psf_hsc = scarlet2.ArrayPSF(psf_hsc_data)
 
 # Load the HST image data
-hst_hdu = fits.open('../scarlet-test-data/data/test_resampling/Cut_HST1.fits')
+hst_hdu = fits.open(os.path.join(data_path, "test_resampling", "Cut_HST1.fits"))
+
 data_hst = hst_hdu[0].data
 wcs_hst = WCS(hst_hdu[0].header)
 channels_hst = ['F814W']
 
 # Load the HST PSF data
-psf_hst = fits.open('../scarlet-test-data/data/test_resampling/PSF_HST.fits')[0].data
+psf_hst = fits.open(os.path.join(data_path, "test_resampling", "PSF_HST.fits"))[0].data
 psf_hst = np.array(psf_hst[None,:,:], np.float32)
 psf_hst = jnp.pad(psf_hst, ((0,0), (1,0), (1,0)))
 psf_hst = np.repeat(psf_hst, 5, 0)
@@ -75,6 +84,10 @@ def test_hst_to_hsc_against_galsim():
     hst_resampled = obs_hsc.render(data_hst)
 
     # Perform the same operations with galsim 
-    out_galsim = np.load('../scarlet-test-data/galsim_hst_to_hsc_resolution.npy')
+    # out_galsim = np.load('../scarlet-test-data/galsim_hst_to_hsc_resolution.npy')
+    out_galsim = np.load(os.path.join(tests_path, "galsim_hst_to_hsc_resolution.npy"))
 
     assert_allclose(out_galsim, hst_resampled[0], atol=1.3e-4)
+
+if __name__=="__main__":
+    test_hst_to_hsc_against_galsim()
