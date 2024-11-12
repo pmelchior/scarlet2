456feb91d51aab73f8cd75de00d727c260a6bd1e
Peter Melchior <peter.m.melchior@gmail.com>
2024-11-12 16:37:29 -0500

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scarlet2',
-    version='0.1.0',    
+    version='0.2.0',    
     description='Scarlet, all new and shiny',
     url='https://github.com/pmelchior/scarlet2',
     author='Peter Melchior',
